024d0b0f00b0c062e0ab3f58d6f938b70ad600b5
Arikaim <info@arikaim.com>
2021-10-12 22:28:14 +0300

    =

---
+++ README.md
@@ -0,0 +1,20 @@
+## Arikaim CMS Redis module
+![1.0.0](https://img.shields.io/github/release/arikaim/redis-module.svg)
+![GPLv3](https://img.shields.io/badge/License-GPLv3-blue.svg)
+
+
+
+Arikaim CMS Redis module
+
+
+
+#### Requirements 
+  * [Arikaim CMS](https://github.com/arikaim/arikaim)
+
+
+
+#### Installation
+
+```sh
+composer require arikaim/redis-module
+```
---
+++ Redis.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Arikaim
+ *
+ * @link        http://www.arikaim.com
+ * @copyright   Copyright (c)  Konstantin Atanasov <info@arikaim.com>
+ * @license     http://www.arikaim.com/license
+ * 
+*/
+namespace Arikaim\Modules\Redis;
+
+use Arikaim\Core\Extension\Module;
+
+/**
+ * Redis module class
+ */
+class Redis extends Module
+{   
+    /**
+     * Install module
+     *
+     * @return void
+     */
+    public function install()
+    {
+        //$this->installDriver('Arikaim\\Modules\\Captcha\\Drivers\\RecaptchaDriver');
+    }
+}
