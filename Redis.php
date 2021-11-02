abb67fe738e56314a1b6b61473967dc6f0bda51f
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 <sales@intersoft.biz>
2021-11-02 18:09:42 +0200

    =

--- Redis.php
+++ Redis.php
@@ -9,6 +9,7 @@
 */
 namespace Arikaim\Modules\Redis;
 
+use Predis\Client;
 use Arikaim\Core\Extension\Module;
 
 /**
@@ -17,12 +18,37 @@ use Arikaim\Core\Extension\Module;
 class Redis extends Module
 {   
     /**
+     * Redis client
+     *
+     * @var object
+     */
+    protected $client;
+
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->setModuleName('redis');
+        $this->client = new Client();
+    }
+
+    /**
+     * Get module instance
+     *
+     * @return mixed|null
+    */
+    public function getInstance()
+    {
+        return $this->client;
+    }
+
+    /**
      * Install module
      *
      * @return void
      */
     public function install()
-    {
-        //$this->installDriver('Arikaim\\Modules\\Captcha\\Drivers\\RecaptchaDriver');
+    {      
     }
 }
--- config/redis-config.php
+++ config/redis-config.php
@@ -7,4 +7,7 @@
 */
 
 return [
+    'scheme' => 'tcp',
+    'host'   => '127.0.0.1',
+    'port'   => 6379
 ];
